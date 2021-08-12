586f84b5831eb080207c7c2d298e6fa53aced450
javier <jibarra@proyectomapache.cl>
2021-08-12 14:26:40 -0400

    solve

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PageController;
 use App\Http\Controllers\DarkModeController;
 use App\Http\Controllers\User\UserController;
+<<<<<<< HEAD
 use App\Http\Controllers\User\UserNoticiaController;
 use App\Http\Controllers\Admin\AdminController;
 
@@ -94,6 +95,7 @@ Route::middleware('auth')->group(function() {
     */
 
     Route::get('/user', [UserController::class, 'index'])->name('index');
+<<<<<<< HEAD
     Route::get('/user/noticia/create', [UserNoticiaController::class, 'create'])->name('create');
     Route::get('/user/noticia/edit', [UserNoticiaController::class, 'edit'])->name('edit');
 
